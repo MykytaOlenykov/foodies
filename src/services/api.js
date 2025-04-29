6211f6b5bea995dd574f227d4d5d1c2fe46442b9
Lada <lada11lada@gmail.com>
2025-04-29 20:06:41 +0200

    api service

---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://mykytaolenykov.com/foodies/api/';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Interceptors: Add auth token if needed
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
