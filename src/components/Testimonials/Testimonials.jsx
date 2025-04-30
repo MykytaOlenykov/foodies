23454b6c0c98da1d2481ca21eeb7b02d2d6c451a
k-zhdanova <k.zhdanova88@gmail.com>
2025-04-30 22:14:19 +0300

    poagination, testimonials, swiper

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,60 @@
+import * as styles from "./Testimonials.module.css";
+import { Typography } from "../Typography/Typography.jsx";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import QuoteImage from '../../assets/images/quote.svg?react';
+import { Pagination, } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const SLIDE_PER_VIEW = 1;
+
+/**
+ * @param {object} props
+ * @param {string} [props.text]
+ * @param {string} [props.author]
+ */
+const TestimonialsCard = ({ text, author }) => {
+  return (
+    <div className={styles.TestimonialsCard}>
+      <QuoteImage className={styles.Testimonials__illustration} />
+      <p className={styles.TestimonialsCard__comment}>{text}</p>
+      <Typography variant="h4">{author}</Typography>
+    </div>
+  );
+};
+
+/**
+ *
+ * @param {object} props
+ * @param {Array<{text: string, author: string}>} props.data
+ */
+const Testimonials = ({data}) => {
+  return (
+    <div className={styles.Testimonials}>
+      <span className={styles.Testimonials__subtitle}>
+        What our customer say
+      </span>
+      <Typography variant="h2">Testimonials</Typography>
+      <Swiper
+        loop
+        modules={[Pagination]}
+        className={styles.Testimonials__swiper}
+        slidesPerView={SLIDE_PER_VIEW}
+        pagination={{
+          clickable: true,
+          horizontalClass: styles.Testimonials__pagination,
+          bulletClass: styles.Testimonials__paginationBullet,
+          bulletActiveClass: styles.active,
+        }}
+      >
+        {data.map((item, index) => (
+          <SwiperSlide>
+            <TestimonialsCard key={index} text={item.text} author={item.author} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export { Testimonials };
