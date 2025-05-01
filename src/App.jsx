3f24fbc4d0c7535938ed8caee409d590da9cc0f8
Ruslan <ruslan.grudistov@gmail.com>
2025-05-01 13:01:53 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-
 import { Route, Routes } from "react-router";
-import { checkApiConnection } from "./services/api.js";
-import { useEffect, useState } from "react";
-import Modal from "./components/Modal/Modal.jsx";
 
 export const App = () => {
-   useEffect(() => {
-    checkApiConnection()
-      .then(() => console.log('✅ API is reachable'))
-      .catch((err) => console.error('❌ API connection failed:', err));
-  }, []);
-
-const [isModalOpen, setIsModalOpen] = useState(false)
-
-const toggleModal = () => {
-  setIsModalOpen((prev)=> {
-    return !prev
-  }); 
-};
-
   return (
-    <div>
-    <button onClick={toggleModal}>click</button>
-      {isModalOpen && <Modal isOpen={isModalOpen} closeModal={toggleModal}>
-        <p>ergjtnrsmtm</p>
-      </Modal> }
-    </div>
+    <Routes>
+      <Route path="/hello-world" element={<div>Hello world!</div>} />
+    </Routes>
   );
 };
