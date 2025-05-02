03769164896c5e4a3712d537292e3fa30a644748
Oleksandr <oleks.bodnar@gmail.com>
2025-05-02 11:07:35 +0300

    add Ingredient Badge
    - add badge to story book
    - add app styles for story book
    - add component Ingredient Badge

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,14 @@
 /** @type { import('@storybook/react').Preview } */
+import "../src/index.css";
+import "../src/variables.css";
+import "../src/preview.css";
+
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
