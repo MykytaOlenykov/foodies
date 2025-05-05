b914f5eae8207ca1b24373c8d855af2acc8c51d4
Yevhen Havrysh <39010905+27ugeen@users.noreply.github.com>
2025-05-05 23:17:02 +0300

    heroImgSet

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,5 @@
 import styles from "./Hero.module.css";
-
-import MobileLarge from "../../assets/images/hero/mobile_big_meat.webp@1x.webp";
-import MobileSmall from "../../assets/images/hero/mobile_small_dessert.webp@1x.webp";
-
-import TabletLarge from "../../assets/images/hero/tablet_big_meat.webp@1x.webp";
-import TabletSmall from "../../assets/images/hero/tablet_small_dessert.webp@1x.webp";
-
-import DesktopLarge from "../../assets/images/hero/desktop_big_meat.webp@1x.webp";
-import DesktopSmall from "../../assets/images/hero/desktop_small_dessert.webp@1x.webp";
+import HeroImageSet from "./HeroImageSet";
 
 const Hero = () => {
   return (
@@ -22,43 +14,7 @@ const Hero = () => {
           <button className={styles.heroButton}>ADD RECIPE</button>
         </div>
 
-        <div className={styles.heroImageWrapper}>
-          {/* Mobile */}
-          <img
-            src={MobileSmall}
-            alt="Dish small mobile"
-            className={`${styles.heroImageSmall} ${styles.mobile}`}
-          />
-          <img
-            src={MobileLarge}
-            alt="Dish large mobile"
-            className={`${styles.heroImageLarge} ${styles.mobile}`}
-          />
-
-          {/* Tablet */}
-          <img
-            src={TabletSmall}
-            alt="Dish small tablet"
-            className={`${styles.heroImageSmall} ${styles.tablet}`}
-          />
-          <img
-            src={TabletLarge}
-            alt="Dish large tablet"
-            className={`${styles.heroImageLarge} ${styles.tablet}`}
-          />
-
-          {/* Desktop */}
-          <img
-            src={DesktopSmall}
-            alt="Dish small desktop"
-            className={`${styles.heroImageSmall} ${styles.desktop}`}
-          />
-          <img
-            src={DesktopLarge}
-            alt="Dish large desktop"
-            className={`${styles.heroImageLarge} ${styles.desktop}`}
-          />
-        </div>
+        <HeroImageSet />
       </div>
     </section>
   );
