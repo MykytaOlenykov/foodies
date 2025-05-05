41bb3640c088c60f825a244b304e6645362ce4d4
ArthurMykhailiuk <artur.myhajlyuk@volo.global>
2025-05-05 13:29:46 +0300

    fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import styles from "./Home.module.css";
+import Hero from "../../components/Hero/Hero";
 import Categories from "../../components/Categories/Categories";
 
 export default function Home() {
@@ -9,7 +10,15 @@ export default function Home() {
         <p className={styles.description}>
           This is a temporary stub for Home Page.
         </p>
-        <Categories />
+        <div>
+          <Hero
+            title="Welcome to the Recipe App"
+            subtitle="Discover delicious recipes from around the world"
+            buttonText="Get Started"
+            onButtonClick={() => console.log("Button clicked!")}
+          />
+          <Categories />
+        </div>
       </div>
     </section>
   );
