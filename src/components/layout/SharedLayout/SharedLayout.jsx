b8ec736e32f9454c6f91ae294b34226f269650f3
Yevhen Havrysh <39010905+27ugeen@users.noreply.github.com>
2025-05-06 18:36:18 +0300

    relocated Hero

--- src/components/layout/SharedLayout/SharedLayout.jsx
+++ src/components/layout/SharedLayout/SharedLayout.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router";
 import styles from "./SharedLayout.module.css";
-import { Header, Hero, Footer, Loader } from "../../../components";
+import { Header, Footer, Loader } from "../../../components";
 import { Suspense } from "react";
 
 const SharedLayout = () => {
   return (
     <div className={styles.container}>
       <Header />
-      <Hero />
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
