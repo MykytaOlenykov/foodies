cb843253a894b79ec5191a5f49adb113ebcbe281
Lada <lada11lada@gmail.com>
2025-05-09 12:56:10 +0200

    additional stories for ButtonIcon.stories.jsx

--- src/components/ButtonIcon/ButtonIcon.stories.jsx
+++ src/components/ButtonIcon/ButtonIcon.stories.jsx
@@ -1,17 +1,26 @@
-import React from "react";
 import { ButtonIcon } from "./ButtonIcon";
-import BurgerMenu from "../../assets/icons/burger-menu.svg?react";
+import HeartIcon  from "../../assets/icons/heart.svg?react";
+import ArrowIcon from "../../assets/icons/arrow-up-right.svg?react";
+import TrashIcon from "../../assets/icons/trash.svg?react";
 
 export default {
-  title: 'Components/ButtonIcon',
+  title: "Components/ButtonIcon",
   component: ButtonIcon,
+  argTypes: {
+    onClick: { action: "clicked" },
+  },
 };
 
-export const WithArrowIcon = () => (
-  <ButtonIcon
-    variant="dark"
-    size="medium"
-    icon={<BurgerMenu width={20} height={20} />}
-    onClick={() => console.log("Icon clicked")}
-  />
+const Template = (args) => <ButtonIcon {...args} />;
+
+export const Variants = () => (
+  <div style={{ display: "flex", gap: "2rem" }}>
+    {["light", "dark"].map((variant) => (
+      <div key={variant} style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+        <ButtonIcon variant={variant} size="small" icon={<HeartIcon />} />
+        <ButtonIcon variant={variant} size="medium" icon={<ArrowIcon />} />
+        <ButtonIcon variant={variant} size="large" icon={<TrashIcon />} />
+      </div>
+    ))}
+  </div>
 );
