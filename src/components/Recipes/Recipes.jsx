54a32e4d96ab8bbf16aa67b88772337a304b1e0e
Mykyta <nikita2001395@gmail.com>
2025-05-09 16:23:31 +0300

    refactor recipes component

--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 
 import Container from "../UI/Container/Container.jsx";
 import RecipeList from "../RecipesMainPage/RecipeList/RecipeList.jsx";
 import { Typography } from "../Typography/Typography.jsx";
+import { selectCategories } from "../../store/categories/selectors.js";
 
 import css from "./Recipes.module.css";
 import ArrowLeftIcon from "../../assets/icons/arrow-left.svg?react";
-import { useSelector } from "react-redux";
-import { selectCategories } from "../../store/categories/selectors.js";
 
 export const Recipes = ({ categoryId }) => {
   const navigate = useNavigate();
@@ -22,27 +22,26 @@ export const Recipes = ({ categoryId }) => {
 
   return (
     <Container className={css.container}>
-      <div className={css.recipesMainBlock}>
-        <div className={css.recipesTitleBlock}>
-          <button
-            className={css.recipesBackButton}
-            type="button"
-            onClick={handleBackClick}
-          >
-            <ArrowLeftIcon className={css.recipesBackIcon} />
-            Back
-          </button>
-          <Typography className={css.recipesCategory} variant="h1">
-            {currentCategory.name}
-          </Typography>
-          <Typography className={css.recipesDescription} variant="p">
-            Go on a taste journey, where every sip is a sophisticated creative
-            chord, and every dessert is an expression of the most refined
-            gastronomic desires.
-          </Typography>
-        </div>
-        <RecipeList categoryId={categoryId} />
+      <div className={css.recipesTitleBlock}>
+        <button
+          className={css.recipesBackButton}
+          type="button"
+          onClick={handleBackClick}
+        >
+          <ArrowLeftIcon className={css.recipesBackIcon} />
+          Back
+        </button>
+        <Typography className={css.recipesCategory} variant="h1">
+          {currentCategory.name}
+        </Typography>
+        <Typography className={css.recipesDescription} variant="p">
+          Go on a taste journey, where every sip is a sophisticated creative
+          chord, and every dessert is an expression of the most refined
+          gastronomic desires.
+        </Typography>
       </div>
+
+      <RecipeList categoryId={categoryId} />
     </Container>
   );
 };
