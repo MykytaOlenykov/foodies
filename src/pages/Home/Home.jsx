92752d186e9d561435f13391891e32d29035215d
Mykyta <nikita2001395@gmail.com>
2025-05-09 15:30:10 +0300

    remove hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,5 @@
-import styles from "./Home.module.css";
-import Hero from "../../components/Hero/Hero";
+// import styles from "./Home.module.css";
 
 export default function Home() {
-  return (
-    <section className={styles.home}>
-      <div className={styles.container}>
-        <div>
-          <Hero
-            title="Improve Your Culinary Talents"
-            subtitle="Amazing recipes for beginners in the world of cooking, enveloping you in the aromas and tastes of various cuisines."
-            buttonText="Add Recipe"
-            onButtonClick={() => console.log("Button clicked!")}
-          />
-          <Categories />
-        </div>
-      </div>
-    </section>
-  );
+  return <></>;
 }
