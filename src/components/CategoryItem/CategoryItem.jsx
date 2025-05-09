2cda18d74ee31a9d36b8650131b9ba55b8dcd4e7
Mykyta <nikita2001395@gmail.com>
2025-05-09 14:58:27 +0300

    remove fragment from category item

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -6,20 +6,18 @@ import { ArrowUpRight } from "lucide-react";
 export default function CategoryItem({ data }) {
   const categoryName = data.name;
   return (
-    <>
-      <div className={`${css.thumb} ${css[categoryName.toLowerCase()]}`}>
-        <div className={css.wrapper}>
-          <p className={css.label}>{data.name}</p>
-          <Link to={`/?category=${data.id}`}>
-            <ButtonIcon
-              className={css.btnIcn}
-              size="medium"
-              type="button"
-              icon={<ArrowUpRight />}
-            />
-          </Link>
-        </div>
+    <div className={`${css.thumb} ${css[categoryName.toLowerCase()]}`}>
+      <div className={css.wrapper}>
+        <p className={css.label}>{data.name}</p>
+        <Link to={`/?category=${data.id}`}>
+          <ButtonIcon
+            className={css.btnIcn}
+            size="medium"
+            type="button"
+            icon={<ArrowUpRight />}
+          />
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
