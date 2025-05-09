bf18e908700ac294c787b3aaa535fa4aaa5f022a
Mykyta <nikita2001395@gmail.com>
2025-05-09 20:53:41 +0300

    fix button in category item

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -1,22 +1,23 @@
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 import { ButtonIcon } from "../ButtonIcon/ButtonIcon";
 import css from "./CategoryItem.module.css";
 import { ArrowUpRight } from "lucide-react";
 
 export default function CategoryItem({ data }) {
   const categoryName = data.name;
+  const navigate = useNavigate();
+
   return (
     <div className={`${css.thumb} ${css[categoryName.toLowerCase()]}`}>
       <div className={css.wrapper}>
         <p className={css.label}>{data.name}</p>
-        <Link to={`/?category=${data.id}`}>
-          <ButtonIcon
-            className={css.btnIcn}
-            size="medium"
-            type="button"
-            icon={<ArrowUpRight />}
-          />
-        </Link>
+        <ButtonIcon
+          className={css.btnIcn}
+          size="medium"
+          type="button"
+          icon={<ArrowUpRight />}
+          onClick={() => navigate(`/?category=${data.id}`)}
+        />
       </div>
     </div>
   );
