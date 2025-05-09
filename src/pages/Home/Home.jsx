72df1d0c3d7a6d3b53e4c015b3de8c3684925e12
Mykyta <nikita2001395@gmail.com>
2025-05-09 20:49:13 +0300

    add data for testimonials

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
+import toast from "react-hot-toast";
+
 import { Category } from "../../components/Category";
 import { Hero } from "../../components/Hero";
 import { Recipes } from "../../components/Recipes";
+import { Testimonials } from "../../components/Testimonials/Testimonials";
+import { getTestimonials } from "../../services/testimonials";
+import { DEFAULT_ERROR_MESSAGE } from "../../constants/common";
+import { normalizeHttpError } from "../../utils";
+
+import css from "./Home.module.css";
 
 const isValidCategory = (category) => {
   if (!category) return false;
@@ -16,11 +25,32 @@ export default function Home() {
   const categoryId =
     isRecipesSubPage && category !== "all" ? Number(category) : null;
 
+  const [testimonials, setTestimonials] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { testimonials } = await getTestimonials();
+        setTestimonials(
+          testimonials.map(({ testimonial, owner }) => ({
+            text: testimonial,
+            author: owner.name,
+          })),
+        );
+      } catch (error) {
+        const { message } = normalizeHttpError(error);
+        toast.error(message ?? DEFAULT_ERROR_MESSAGE);
+      }
+    })();
+  }, []);
+
   return (
-    <>
+    <div className={css.container}>
       <Hero />
 
       {isRecipesSubPage ? <Recipes categoryId={categoryId} /> : <Category />}
-    </>
+
+      <Testimonials data={testimonials} />
+    </div>
   );
 }
