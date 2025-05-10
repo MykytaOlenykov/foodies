1128b6157aea6e646258b92f3eab53ae50ce80b9
Mykyta <nikita2001395@gmail.com>
2025-05-10 16:08:54 +0300

    rm popular recipes component

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -1,12 +1,10 @@
 import Container from "../../components/UI/Container/Container";
 import RecipeInfo from "../../components/RecipeInfo/RecipeInfo";
-import PopularRecipes from "../../components/PopularRecipes/PopularRecipes";
 
 const RecipePage = () => {
   return (
     <Container>
       <RecipeInfo />
-      <PopularRecipes />
     </Container>
   );
 };
