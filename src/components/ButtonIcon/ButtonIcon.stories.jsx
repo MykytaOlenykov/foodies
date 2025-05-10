3fccab41fdb07853c348f34c98078cef2a92a0e9
k-zhdanova <k.zhdanova88@gmail.com>
2025-05-10 16:13:59 +0300

    transparent button icon added

--- src/components/ButtonIcon/ButtonIcon.stories.jsx
+++ src/components/ButtonIcon/ButtonIcon.stories.jsx
@@ -8,10 +8,28 @@ export default {
 };
 
 export const WithArrowIcon = () => (
-  <ButtonIcon
-    variant="dark"
-    size="medium"
-    icon={<BurgerMenu width={20} height={20} />}
-    onClick={() => console.log("Icon clicked")}
-  />
+  <div style={{ display: "flex", flexDirection: "column", gap: "1rem"  }}>
+    <div style={{ display: "flex", gap: "1rem" }}>
+      <ButtonIcon
+        variant="light"
+        size="medium"
+        icon={<BurgerMenu width={20} height={20} />}
+        onClick={() => console.log("Icon clicked")}
+      />
+      <ButtonIcon
+        variant="dark"
+        size="medium"
+        icon={<BurgerMenu width={20} height={20} />}
+        onClick={() => console.log("Icon clicked")}
+      />
+    </div>
+    <div style={{ backgroundColor: "#8f8f8f", padding: "2rem", width: 'fit-content' }}>
+      <ButtonIcon
+        variant="transparent"
+        size="medium"
+        icon={<BurgerMenu width={20} height={20} />}
+        onClick={() => console.log("Icon clicked")}
+      />
+    </div>
+  </div>
 );
